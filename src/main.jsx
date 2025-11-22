34f5f38a77487dee26532f34200ca0fa21ab213d
bilalsk111 <bilalshaikj6@gmail.com>
2025-11-22 22:13:18 +0530

    cleanup & fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-// import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import Stairs from './components/common/Stairs.jsx';
-import NavContext from './context/NavContext.jsx';
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Stairs from './components/common/Stairs.jsx'
+import NavContext from './context/NavContext.jsx'
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-<BrowserRouter basename="/k72/">
-      <Stairs>
-        <NavContext>
-          <App />
-        </NavContext>
-      </Stairs>
-    </BrowserRouter>
+  <BrowserRouter  basename="/">
+    <Stairs>
+      <NavContext>
+        <App />
+      </NavContext>
+    </Stairs>
+  </BrowserRouter>
 );
