c82922566411545485d4408b308b62026b354f14
bilalsk111 <bilalshaikj6@gmail.com>
2025-11-22 22:00:31 +0530

    fix node engine for vercel

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='w-full h-full'>
-        <video className='w-full h-full object-cover' loop autoPlay muted src="/public/herosectionvideo.mp4"></video>
+        <video className='w-full h-full object-cover' loop autoPlay muted src="./public/herosectionvideo.mp4"></video>
     </div>
   )
 }
